6fa1a059bc5d64d53c9b5269d7424c08783b16a8
a29cute <allsun0829@gmail.com>
2024-09-23 10:26:34 +0900

    연락처 기능

---
+++ src/contact/AddressBook.java
@@ -0,0 +1,37 @@
+package contact;
+
+import java.util.*;
+public class AddressBook {
+    private List<Contact> contacts = new ArrayList<>();
+
+    public AddressBook() {
+        this.contacts = new ArrayList<>();
+    }
+
+    public void addContact(Contact contact) {
+        this.contacts.add(contact);
+    }
+
+    public void displayContacts() {
+        if (contacts.isEmpty()) {
+            System.out.println("연락처가 비어있습니다.");
+        } else {
+            for (Contact contact : contacts) {
+                System.out.println(contact);
+            }
+        }
+    }
+
+    public void searchContact(String name) {
+        boolean found = false;
+        for (Contact contact : contacts) {
+            if (contact.getName().equalsIgnoreCase(name)) {
+                System.out.println(contact);
+                found = true;
+            }
+        }
+        if (!found) {
+            System.out.println("연락처를 찾을 수 없습니다.");
+        }
+    }
+}
